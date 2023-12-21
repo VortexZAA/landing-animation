9f94dbb637ae5aea6ad3b29205b84d56ac3138bc
VortexZAA <abidinayhan94@gmail.com>
2023-12-21 18:59:11 +0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="One 4 Global" />
+        <meta name="description" content="FACI Protocol" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="keywords" content="One 4 Global" />
+        <meta name="keywords" content="FACI Protocol" />
       </Head>
       <title>FACI Protocol</title>
       <body className="max-w-[100vw] overflow-x-hidden bg-black ">

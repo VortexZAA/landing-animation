026825db2a367cbc55e9ea5d640910e2cfeddbb9
VortexZAA <abidinayhan94@gmail.com>
2023-12-26 18:01:52 +0300

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -102,7 +102,7 @@ export default function Header() {
         </div>
       </header>
       <ul
-        className="flex lg:hidden z-40 flex-col fixed  bg-black lg:bg-transparent lg:relative  top-0 left-0 items-center justify-center lg:justify-end  lg:h-auto lg:flex-row lg:mt-0 text-xl font-normal w-full lg:w-1/2 gap-6  transition-all h-screen overflow-y-auto"
+        className="flex lg:hidden z-40 flex-col fixed  bg-black lg:bg-transparent lg:relative  top-0 left-0 items-center justify-center lg:justify-end  lg:h-auto lg:flex-row lg:mt-0 text-xl font-normal w-full lg:w-1/2 gap-6 text-white transition-all h-screen overflow-y-auto"
         style={{
           transform: showMobile ? "translateX(0)" : "translateX(100%)",
           transition: "all 0.3s ease-in-out",
@@ -118,7 +118,7 @@ export default function Header() {
                 href={item.link}
                 target={item.target}
                 onClick={() => setShowMobile(!showMobile)}
-                className={`flex justify-center items-center py-0  lg:border-0 lg:hover:text-orange-400 transition-colors cursor-pointer text-center w-full ${item?.addClass} `}
+                className={` justify-center items-center py-0 !text-white lg:border-0 lg:hover:text-orange-400 transition-colors cursor-pointer text-center w-full ${item?.addClass} `}
               >
                 {item.title}
               </Link>
@@ -130,6 +130,12 @@ export default function Header() {
             </li>
           );
         })}
+        <li className="flex flex-col md:hidden group text-white justify-center items-center py-0 -mt-2  lg:border-0 lg:hover:text-orange-400 transition-colors cursor-pointer text-center w-full relative">
+          <button className=" justify-center items-center peer  lg:border-0 lg:hover:text-orange-400  cursor-pointer text-center w-fullborder-2 border-white rounded-full px-6 py-2 bg-white text-black md:text-white md:bg-transparent hover:bg-white hover:text-black transition-colors font-semi-bold flex">
+            Launch App
+          </button>
+          <span className="hidden peer-hover:block text-xs group-hover:flex absolute -bottom-5 md:-bottom-2  md:hidden text-orange-400 font-bold">Mobile is not supported.</span>
+        </li>
       </ul>
     </>
   );
@@ -180,6 +186,6 @@ const menu = [
     link: "/buy-badge",
     target: "_self",
     addClass:
-      "border-2 border-white rounded-full px-6 py-2 bg-white text-black md:text-white md:bg-transparent hover:bg-white hover:text-black transition-colors font-semi-bold",
+      "border-2 border-white rounded-full px-6 py-2 bg-white text-black md:text-white md:bg-transparent hover:bg-white hover:text-black transition-colors font-semi-bold hidden md:flex ",
   },
 ];

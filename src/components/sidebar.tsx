ab76f2c39148de1d38c1b2eb8f1d9315d42abec6
VortexZaa <abidinayhan94@gmail.com>
2024-01-08 20:12:55 +0300

    update

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -493,7 +493,7 @@ export default function SideBar() {
     {
       id: 8,
       name: "Bridge ",
-      path: "https://bswap.bevm.io/#/bridge",
+      path: "https://bevm.io/bridge",
       target: "_blank",
       icon: (
         <svg
@@ -506,7 +506,7 @@ export default function SideBar() {
           <path d="M32 32C14.3 32 0 46.3 0 64S14.3 96 32 96H72v64H0V288c53 0 96 43 96 96v64c0 17.7 14.3 32 32 32h32c17.7 0 32-14.3 32-32V384c0-53 43-96 96-96s96 43 96 96v64c0 17.7 14.3 32 32 32h32c17.7 0 32-14.3 32-32V384c0-53 43-96 96-96V160H504V96h40c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM456 96v64H376V96h80zM328 96v64H248V96h80zM200 96v64H120V96h80z" />
         </svg>
       ),
-      status: true,
+      status: chainId === "0x5dd",
     },
     {
       id: 9,

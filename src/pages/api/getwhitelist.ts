1c6e609cdc367ca52cb480c2818220d9fca74582
VortexZaa <abidinayhan94@gmail.com>
2024-01-15 13:53:45 +0300

    update

---
+++ src/pages/api/getwhitelist.ts
@@ -0,0 +1,63 @@
+import pb from "@/lib/pocketbase";
+import type { NextRequest } from "next/server";
+
+export const runtime = "edge";
+/* export const config = {
+  runtime: "edge",
+}; */
+export default async function handler(req: NextRequest) {
+  // HTTP istek metodunu kontrol edin
+  const address =  req.nextUrl.searchParams.get("address") || ""
+  if (req.method !== "GET") {
+    // POST olmayan istekler için 405 Method Not Allowed yanıtı döndür
+    return new Response(JSON.stringify({ message: "Method not allowed" }), {
+      status: 405,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  try {
+    // İstek gövdesini JSON olarak ayrıştır
+    
+    let data;
+    const records = await pb.collection('soulbound_whitelist').getFullList({
+        sort: '-created',
+        filter: address && `address="${address}"`
+    }).then((res) => {
+        data = res;
+    }
+    )
+
+    return new Response(
+      JSON.stringify({
+        message: "Success",
+        data: data,
+        address: address
+      }),
+      {
+        status: 200, // Başarılı işlem
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error: any) {
+    // Hata durumunda 500 Internal Server Error yanıtı döndür
+    return new Response(
+      JSON.stringify({
+        message: "Internal Server Error",
+        error: error?.message,
+        address: address
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
+

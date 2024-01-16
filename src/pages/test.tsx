619705aa7e7c2799c2bc31c841b5f4bc170ed185
VortexZaa <abidinayhan94@gmail.com>
2024-01-16 17:51:45 +0300

    update

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,19 +1,26 @@
 //npm install @splinetool/react-spline @splinetool/runtime bu komutları çalıştırıyoruz
 import Spline from "@splinetool/react-spline"; // bunu ekleyeceğiz
-import addresData from "@/data/calimBadge/4.json"; // bunu ekleyeceğiz
+import AddresData from "@/data/calimBadge/1.json"; // bunu ekleyeceğiz
+import AddresData2 from "@/data/calimBadge/2.json"; // bunu ekleyeceğiz
+import AddresData3 from "@/data/calimBadge/3.json"; // bunu ekleyeceğiz
+import AddresData4 from "@/data/calimBadge/4.json"; // bunu ekleyeceğiz
 import pb from "@/lib/pocketbase";
 import { useEffect, useState } from "react";
 export default function TEST() {
   const [totalCreate, setTotalCreate] = useState(0);
-
+  const addresData = AddresData3;
   async function createCalimBadge() {
     let totalCreate = 0;
+    console.log("Address sayısı", addresData.length);
+    
     await addresData.forEach(async (element) => {
       try {
         const checkAddress = await pb
           .collection("claim_badge_new")
           .getFirstListItem(`address="${element.address}"`)
-          .then((res) => {return true})
+          .then((res) => {
+            return true;
+          })
           .catch((err) => {
             console.log(err);
             return false;
@@ -23,7 +30,7 @@ export default function TEST() {
             .collection("claim_badge_new")
             .create({ address: element.address });
           console.log(result);
-          totalCreate = totalCreate + 1
+          totalCreate = totalCreate + 1;
         } else {
           console.log(element.address, " zaten var");
         }
@@ -40,6 +47,7 @@ export default function TEST() {
   return (
     <div className="mx-auto z-20 absolute ">
       <h2 className="text-white text-7xl z-20">Total Create: {totalCreate}</h2>
+      <h3 className="text-white text-6xl z-20">Address sayısı: {addresData.length}</h3>
     </div>
   );
 }

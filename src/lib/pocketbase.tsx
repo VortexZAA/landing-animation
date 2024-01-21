6223c62f5caf14c14ebcb368af12fa134f07c75b
VortexZaa <abidinayhan94@gmail.com>
2024-01-21 22:29:42 +0300

    update

--- src/lib/pocketbase.tsx
+++ src/lib/pocketbase.tsx
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase("https://db.aeroscraper.io");
+
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL || "https://db.aeroscraper.io");
 
 pb.autoCancellation(false);
 
